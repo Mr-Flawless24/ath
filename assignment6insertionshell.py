bae5730d2760f6fc2e4f5f86be7fad0de1fc0637
MrFlawless <prakashkaswar17@gmail.com>
2024-12-12 22:37:20 +0530

    Update assignment6insertionshell.py

--- assignment6insertionshell.py
+++ assignment6insertionshell.py
@@ -1,22 +1,41 @@
-a=int(input("Enter the number of student in se=="))
-se=[]
-for i in range (0,a):
-  b=float(input("Enter the percentage== "))
-  se.append(b)
-print(se)
+percentage = []
+n = int(input("Enter the number of students in class: "))
+for i in range(n):
+    k = float(input(f"Enter the percentage of student {i + 1}: "))
+    percentage.append(k)
 
+# Insertion sort
+def insertionsort(percentage):
+    for i in range(1, len(percentage)):
+        key = percentage[i]
+        j = i - 1
 
-def insertion(se,a):
-  for i in range (1,a):
-    key=se[i]
-    j=i-1
-    while((se[j]>key)and(j>=0)):
-      temp=se[j]
-      se[j]=se[j+1]
-      se[j+1]=temp
-      j=j-1
-      se[j+1]=key
-  print("sorted list== ",se)  
-    
-insertion(se,a)
-        
+        while j >= 0 and key < percentage[j]:
+            percentage[j + 1] = percentage[j]
+            j -= 1
+        percentage[j + 1] = key
+
+    return percentage
+
+print("Insertion sorted array:", insertionsort(percentage))
+
+# Shell sort
+def shellsort(percentage):
+    gap = len(percentage) // 2
+
+    while gap > 0:
+        for j in range(gap, len(percentage)):
+            temp = percentage[j]
+            i = j
+
+            while i >= gap and percentage[i - gap] > temp:
+                percentage[i] = percentage[i - gap]
+                i -= gap
+
+            percentage[i] = temp
+
+        gap //= 2
+
+    return percentage
+
+print("Shell sorted array:", shellsort(percentage))
